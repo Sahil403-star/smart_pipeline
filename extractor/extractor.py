6c7739dc34b6d4201a9fbca537386457865451cf
Sahil Srivastava <sahil.srivastava401@gmail.com>
2026-04-24 17:48:12 +0530

    Added config-driven extraction using selectors JSON

---
+++ extractor/extractor.py
@@ -0,0 +1,21 @@
+def extract_field(item, selector):
+    if "::attr(" in selector:
+        tag, attr = selector.split("::attr(")
+        attr = attr.replace(")", "")
+        element = item.select_one(tag)
+        return element[attr] if element else None
+    else:
+        element = item.select_one(selector)
+        return element.text if element else None
+
+
+def extract(items, config):
+    data = []
+
+    for item in items:
+        obj = {}
+        for field, selector in config["fields"].items():
+            obj[field] = extract_field(item, selector)
+        data.append(obj)
+
+    return data
